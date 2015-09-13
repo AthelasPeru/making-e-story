09dfbb85068cac12ee9c1796f2f5c590f4c66f33
Pedro Baumann <ondoheer@gmail.com>
2015-09-12 23:44:57 -0500

    formullario completo

--- app/models.py
+++ app/models.py
@@ -9,3 +9,7 @@ class Game(mongo.DynamicDocument):
 	we don't really have time right now to set each field.
 	"""
 	json_data = mongo.DictField()
+	general_data = mongo.DictField()
+	character_data = mongo.DictField()
+	phases = mongo.ListField()
+	
