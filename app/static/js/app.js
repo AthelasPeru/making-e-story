1c35b48b378ad76fff9fa26d462ea906e2ac7ae2
Pedro Baumann <ondoheer@gmail.com>
2015-09-13 00:00:13 -0500

    procesando el formulario

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -24,6 +24,46 @@ $(document).ready(function(){
 			age: $("#char_age").val()
 		}
 	});
+
+	var phases = [];
+
+	$("#addPhase").on("click", function(){
+		var phase = {
+			name: $("#phase_name").val(),
+			description: $("#phase_text").val(),
+			success: {
+				req:{
+					$("#req#key").val(): $("#req#val").val(),
+				}, 
+			},
+			failure:{},
+			actions: []
+
+		}
+
+		// add phase to phases array
+		phases.push(phase);
+	});
+
+
+	$("#addAction").on("click", function(){
+		var action = {
+			name: $("#action_name").val(),
+			time_tick: $("#time_tick").val(),
+			messages:[
+				$("#action_msg_1").val(),
+				$("#action_msg_2").val(),
+				$("#action_msg_3").val()
+			],
+			resource:{
+				name: $("#resource_name").val(),
+				value: $("#resource_value")
+			}
+		}
+
+		// agregamos acción a la última fase
+		phases[phases.length -1].actions.push(action);
+	});
 });
 
 
