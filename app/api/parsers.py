ad4a24c957984bb646b1d06312527743b0b09221
Pedro Baumann <ondoheer@gmail.com>
2015-09-12 19:41:30 -0500

    api casi listo

--- app/api/parsers.py
+++ app/api/parsers.py
@@ -2,3 +2,10 @@ from flask_restful import reqparse
 
 
 game_post_parser = reqparse.RequestParser(bundle_errors=True)
+game_post_parser.add_argument(
+	"json_data",
+	type=dict,
+	required=True,
+	location=["json"],
+	help="No data provided"
+)
--- app/config.py
+++ app/config.py
@@ -1,24 +1,80 @@
-def Config(object):
-	SECRET_KEY = os.environ.get(
-		"SECRET_KEY", "saddasjd348o43rfW?W=WEDW338XDIQWYDWHDX3!!"
-	)
+import os
 
 
-def DevConfig(Config):
-	DEBUG = True
-	MONGODB_SETTINGS = {
-		"db": "making_e_dev"
-	}
+class Config(object):
+    SECRET_KEY = os.environ.get(
+        'SECRET_KEY', "ASSDSSADASD788632ZSSCS7sasd78dad33dyas89ds7dsa")
 
 
-def ProdConfig(Config):
-	DEBUG = False
-	MONGODB_SETTINGS = {
-		"db": "making_e_prod"
-	}
+class DevConfig(Config):
+    DEBUG = True
+    
+    MONGODB_SETTINGS = {
+        'db': 'making_e__dev'
+    }
+    ASSETS_DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    DEBUG_TB_PANELS = (
+        'flask.ext.mongoengine.panels.MongoDebugPanel',
+        'flask_debugtoolbar.panels.versions.VersionDebugPanel',
+        'flask_debugtoolbar.panels.timer.TimerDebugPanel',
+        'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
+        'flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel',
+        'flask_debugtoolbar.panels.config_vars.ConfigVarsDebugPanel',
+        'flask_debugtoolbar.panels.template.TemplateDebugPanel',
+        'flask_debugtoolbar.panels.logger.LoggingPanel',
+        'flask_debugtoolbar.panels.route_list.RouteListDebugPanel',
+        'flask_debugtoolbar.panels.profiler.ProfilerDebugPanel'
+    )
+    
 
+
+
+
+class HerokuConfig(Config):
+    MONGODB_SETTINGS = {
+        'host': 'mongodb://heroku_app36432742:5t3gl9m5obtg6d9ma3snidsba5@ds031632.mongolab.com:31632/heroku_app36432742'
+    }
+    ASSETS_DEBUG = False
+
+class HerokuDevConfig(HerokuConfig):
+    DEBUG = True
+    ASSETS_DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    DEBUG_TB_PANELS = (
+        'flask.ext.mongoengine.panels.MongoDebugPanel',
+        'flask_debugtoolbar.panels.versions.VersionDebugPanel',
+        'flask_debugtoolbar.panels.timer.TimerDebugPanel',
+        'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
+        'flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel',
+        'flask_debugtoolbar.panels.config_vars.ConfigVarsDebugPanel',
+        'flask_debugtoolbar.panels.template.TemplateDebugPanel',
+        'flask_debugtoolbar.panels.logger.LoggingPanel',
+        'flask_debugtoolbar.panels.route_list.RouteListDebugPanel',
+        'flask_debugtoolbar.panels.profiler.ProfilerDebugPanel'
+    )
+
+class ProdConfig(Config):
+    MONGODB_SETTINGS = {
+        'db': "making_e"
+    }
+    ASSETS_DEBUG = False
+
+class TestConfig(Config):
+    MONGODB_SETTINGS = {
+        'db': 'test_making_e'
+    }
+
+    DEBUG = True
+    DEBUG_TB_ENABLED = False
+    WTF_CSRF_ENABLED = False
+
+# mira esto
 config = {
-	"dev": DevConfig,
-	"prod": ProdConfig
-	
+    "default": DevConfig,
+    "development": DevConfig,
+    "production": ProdConfig,
+    "heroku": HerokuConfig,
+    "heroku_dev": HerokuDevConfig,
+    "testing": TestConfig
 }
